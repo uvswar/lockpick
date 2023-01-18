5d558904bd507e600b9625cea45fce0636bbda66
Don <36248274+Donthelosr@users.noreply.github.com>
2023-01-17 19:42:49 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+author 'Donthelosr'
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles.css',
+    'html/script.js'
+}
+
+client_script 'client.lua'
